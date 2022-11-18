f5571ea8e11984e57f265a10ca536dbb66076f15
UnipireOff <cassidy.off@outlook.com.au>
2022-11-18 17:33:14 +0100

    Core Update

---
+++ data.py
@@ -0,0 +1,20 @@
+# </ data.py />
+namesm = 'LumiaPSS'                                         #In use
+namelg = 'LumiaPythonScriptSystem'
+wmsg1 = 'Hello! This is early development version of '      #In use
+wmsg1end = '!' #Delete this '!' if you want to              #In use
+wmsg2 = 'Please report issues at: '                         #In use
+git = 'https://github.com/UnipireOff/LumiaPSS'
+gitissues = 'https://github.com/UnipireOff/LumiaPSS/issues' #In use
+gitpers = 'https://github.com/UnipireOff'
+
+#</ ------------------------------------------------------------ />
+
+#</ custom url or data />
+# To use custom github url
+# replace empty space with url.
+gitc = ''
+gitissuesc = '' #In use
+gitpersc = ''
+namesmc = ''    #In use
+namelg = ''
