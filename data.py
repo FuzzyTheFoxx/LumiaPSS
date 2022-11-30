b3e3542d2a542fa4c1690d390808f89f8a9757a2
Fuzzy <cassidy.off@outlook.com.au>
2022-11-30 19:27:22 +0100

    core

--- data.py
+++ data.py
@@ -4,9 +4,9 @@ namelg = 'LumiaPythonScriptSystem'
 wmsg1 = 'Hello! This is early development version of '      #In use
 wmsg1end = '!' #Delete this '!' if you want to              #In use
 wmsg2 = 'Please report issues at: '                         #In use
-git = 'https://github.com/UnipireOff/LumiaPSS'
-gitissues = 'https://github.com/UnipireOff/LumiaPSS/issues' #In use
-gitpers = 'https://github.com/UnipireOff'
+git = 'https://github.com/FuzzyTheFoxx/LumiaPSS'
+gitissues = 'https://github.com/FuzzyTheFoxx/LumiaPSS/issues' #In use
+gitpers = 'https://github.com/FuzzyTheFoxx'
 
 #</ ------------------------------------------------------------ />
 
