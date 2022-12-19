cad3951a2beddc6092ed2f36e8185ebcf187c5d2
Fuzzy <cassidy.off@outlook.com.au>
2022-12-19 22:36:02 +0100

    ;

---
+++ #Dev/CorePSS/_classes_csl_.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+#
+#  __init__.pss
+#
+#  Copyright 2022  <FuzzyTheFoxx>
+#
+#  This program is free software# you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation# either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY# without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program# if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#
+#
+#------------------------------------------------------------------
+#[Extension] [Long Name]      [More Info]
+#*.pss        PSS Exec.        Usually for initialization
+#*.pexec      PSS Exec.        Usually for programs
+#*.pss-lib    PSS Lib.         For storing data, ex: files
+#*.pss-config PSS Config.      Configuration files
+#*.py         Python Script    Usually for variables
+#*.lnks       Internet Links   Storeng links
+#*.pss-inst   Install info     For storing data ex: sys. fldr. name
+#*.ptxt       Python Text      Text file but better (?)
+
+#[nameslg]
+lpss='PSS Init. File'
+lpexec='PSS Executable'
+lpsslib='PSS Library'
+lpssconf='PSS Config. File'
+lpy='Python Script System'
+llnks='Internet Link Library'
+lpssinst='PSS Instalation Info.'
+lptxt='Advanced Text File'
+
+#[nameshrt]
+spss='PSS Init.'
+spexec='Exec-able'
+spsslib='Lib.'
+spssconf='Config.'
+spy='Script'
+slnks='Links Lib'
+spssinst='Inst. Info.'
+sptxt='Adv. txt'
+
